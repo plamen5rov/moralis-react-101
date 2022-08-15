c8d6c1f781932536476d6455e31ebfa369ff8e12
Plamen <biznetmen@gmail.com>
2022-08-15 13:23:45 +0300

    basic react component added and styled

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import logo from './plamen.jpeg';
 import './App.css';
+import Coin from './components/Coin/Coin'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            COIN EXCHANGE APP
+          </p>
+        </div>
+        <div>
+          <a
+            className="App-link"
+            href="https://github.com/plamen5rov"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My GitHub Profile
+          </a>
+        </div>
+
+
       </header>
+      <table className='coin-table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price="$60,000" />
+          <Coin name="Ethereum" ticker="ETH" price="$20,000" />
+        </tbody>
+      </table>
     </div>
   );
 }
