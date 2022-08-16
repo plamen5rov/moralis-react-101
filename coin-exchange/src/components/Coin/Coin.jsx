d7fd377c200547c8ee1c6925d2ecdb278a84003f
Plamen <biznetmen@gmail.com>
2022-08-16 10:44:20 +0300

    price change by state added

--- coin-exchange/src/components/Coin/Coin.jsx
+++ coin-exchange/src/components/Coin/Coin.jsx
@@ -3,12 +3,36 @@ import './Coin.css';
 import PropTypes from 'prop-types';
 
 export default class Coin extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            price: this.props.price
+        }
+    }
+
+    componentDidMount() {
+        const callback = () => {
+            // seting state to a random value
+            const randomPercentage = 0.995 + Math.random() * 0.1;
+
+            this.setState(function (oldState) {
+                return {
+                    price: oldState.price * randomPercentage
+                };
+            });
+        }
+        setInterval(callback, 1000);
+    }
+
+
+
     render() {
         return (
             <tr className='coin-row'>
                 <td>{this.props.name}</td>
                 <td>{this.props.ticker}</td>
-                <td>${this.props.price}</td>
+                <td>${this.state.price}</td>
             </tr>
         )
     }
